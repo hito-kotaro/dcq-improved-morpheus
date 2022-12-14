6dd7c2e003f3ad940a605ae5437dc1ff00e20d19
hito-kotaro <tohi.work@gmail.com>
2022-12-14 22:47:28 +0900

    typeormの接続設定作成

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const source = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USERS,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: ['src/entity/*.entity.ts'],
+  migrations: ['src/migration/**/*.ts'],
+});
+
+export default source;
