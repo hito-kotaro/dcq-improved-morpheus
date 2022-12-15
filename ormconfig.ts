962c3d93d38d4321479f337549b92ba93ace6cc3
hito-kotaro <tohi.work@gmail.com>
2022-12-16 08:24:10 +0900

    環境編数名の修正

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,7 +7,7 @@ const source = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USERS,
+  username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   entities: ['src/entity/*.entity.ts'],
