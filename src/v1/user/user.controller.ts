3949791e4f932407d706a7480cafdb58298b8f69
hito-kotaro <tohi.work@gmail.com>
2022-12-16 11:59:43 +0900

    テスト用のエンドポイント追加

--- src/v1/user/user.controller.ts
+++ src/v1/user/user.controller.ts
@@ -6,6 +6,11 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Post('/test/register')
+  async testSignUp(@Body() event: any) {
+    return event;
+  }
+
   @Post('/register')
   async signUp(@Body() user: { id: string }): Promise<Users> {
     return await this.userService.register(user.id);
